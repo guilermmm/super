12949f13450ddf6ceb5aa281f1f919def1b9ea6f
Guilherme <62029246+guilermmm@users.noreply.github.com>
2021-10-16 23:35:02 -0300

    model.vo
    arrumando as branches pq fiz errado

---
+++ InvoiceItemsVO.java
@@ -0,0 +1,28 @@
+package model.vo;
+
+public class InvoiceItemsVO {
+    private ItemVO item;
+    private int quantity;
+
+    public ItemVO getItem() {
+        return this.item;
+    }
+
+    public void setItem(ItemVO item) {
+        if (item != null) {
+            this.item = item;
+        }
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity < 0) {
+            System.out.println("A quantidade não pode ser menor que zero");
+        } else {
+            this.quantity = quantity;
+        }
+    }
+}
